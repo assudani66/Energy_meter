2dc7967bba7a7e03b745254411f442e87719acfd
assudani66 <assudani66@gmail.com>
2022-10-14 17:32:03 +0530

    Return span

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-
+var energy_remaining = 100
+var totalEnergy = 500
 function App() {
-  // return ();
+  return (<span>{energy_remaining}/{totalEnergy}</span>);
 }
 
 export default App;
