1cdd8f423653b2a29e1c95bf918b2ade3acbd617
assudani66 <assudani66@gmail.com>
2022-10-15 12:07:07 +0530

    Showcase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import energyContext from './Components/energyContext';
 var energy_remaining = 100
 var totalEnergy = 500
 function App() {
-  return (<span>{energy_remaining}/{totalEnergy}</span>);
+  return (<span>{energy_remaining}/{totalEnergy}<span>Design a Life you Love</span></span>);
+  <energyContext.Provider value={energy}>
+    
+  </energyContext.Provider>
 }
 
 export default App;
---
+++ src/Components/energyContext.js
@@ -0,0 +1,4 @@
+import React , { createContext } from "react";
+
+const energyContext = React.createContext()
+export default energyContext
