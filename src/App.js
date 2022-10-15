ff921207e5206bb67b5dbfde533a796e8083ff2c
assudani66 <assudani66@gmail.com>
2022-10-15 12:29:44 +0530

    introduce div

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import energyContext from './Components/energyContext';
 var energy_remaining = 100
 var totalEnergy = 500
 function App() {
-  return (<span>{energy_remaining}/{totalEnergy}<span>Design a Life you Love</span></span>);
+  return (<div>
+    <span>{energy_remaining}/{totalEnergy}</span>
+    <span>Design a Life you Love</span>
+    </div>);
 }
 
 export default App;
