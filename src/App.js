c855f1be2fd5dd2edd3cae5ccc9c26f58410df0c
assudani66 <assudani66@gmail.com>
2022-10-15 13:55:56 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 var energy_remaining = 100
 var totalEnergy = 500
 function App() {
-  return (<div>
+  return (
+  <div> 
     <span>{energy_remaining}/{totalEnergy}</span>
     <span>Design a Life you Love</span>
-    </div>);
+  </div>);
 }
+
 
 export default App;
