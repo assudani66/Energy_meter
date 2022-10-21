23eb3d2f3c80e7eeea2dced8f922db52ae04a80f
assudani66 <assudani66@gmail.com>
2022-10-21 12:18:57 +0530

    added energy Logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import React ,{useState} from 'react';
+import { useContext } from 'react';
+import energyContext from './Components/energyContext';
 var energy_remaining = 100
 var totalEnergy = 500
 function App() {
+  const [energyRemaining,setEnergyremaing] = useState(100)
+  const energyCalculator = () =>{
+    setEnergyremaing(energyRemaining - 25)
+    console.log("Clicked")
+  }
   return (
   <div> 
-    <span>{energy_remaining}/{totalEnergy}</span>
+    <span>{energyRemaining}/{totalEnergy}</span>
     <span>Design a Life you Love</span>
+    <br></br>
+    <button onClick={energyCalculator}>Spend</button>
   </div>);
 }
 
